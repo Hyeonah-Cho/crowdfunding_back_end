c5bbe6d8cf62772305cdef63bbd0b91c350ef73b
Hyeonah-Cho <hyeonah.cho.au@gmail.com>
2025-11-11 22:46:13 +1000

    Add: serializer

--- crowdfunding/crowdfunding/settings.py
+++ crowdfunding/crowdfunding/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    # 'django_extensions',
     'fundraisers.apps.FundraisersConfig',
     'rest_framework',
     "django.contrib.admin",
--- crowdfunding/fundraisers/models.py
+++ crowdfunding/fundraisers/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 
 class Fundraiser(models.Model):
+    # each variable is a column in the database and its type (id is given by Django by default)
     title = models.CharField(max_length=200)
     description = models.TextField()
     goal = models.IntegerField()
