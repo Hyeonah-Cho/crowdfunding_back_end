503f611fbe4bf31c694ebb150467ec3608e5c642
Hyeonah Cho <hyeonah.cho.au@gmail.com>
2026-01-31 11:31:44 +1000

    Edit: Permission for superuser

--- crowdfunding/fundraisers/permissions.py
+++ crowdfunding/fundraisers/permissions.py
@@ -4,4 +4,8 @@ class IsOwnerOrReadOnly(permissions.BasePermission):
     def has_object_permission(self, request, view, obj):
         if request.method in permissions.SAFE_METHODS:
             return True
+        
+        if request.user and request.user.is_authenticated and request.user.is_superuser:
+            return True
+        
         return obj.owner == request.user # Q: If this permission class is self-made, how does DRF know where to get owner and user from??
